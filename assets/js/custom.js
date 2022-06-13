dbd9a6334a5158c77e2f0101a21a8b2994035248
claracena <claracena@users.noreply.github.com>
2022-06-13 18:30:25 -0300

    posteo y login

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,6 +24,10 @@ $( document ).ready(function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+    var quill = new Quill('#editor', {
+        theme: 'snow'
+    });
+
 });
 
 var mybutton = document.getElementById("myBtn");
