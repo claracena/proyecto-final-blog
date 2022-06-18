8002ee3616df27614d3e965c93ca66ad1bb87d8d
joseorozco84 <joseorozco84@gmail.com>
2022-06-17 21:50:33 -0300

    blog branch

--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -77,21 +77,27 @@ WSGI_APPLICATION = 'gamerblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         # 'OPTIONS': {
+#         #     'service': 'gamerblog',
+#         #     'passfile': '.pgpass',
+#         # },
+#         'NAME': 'gamerblog',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Genius2008',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'OPTIONS': {
-        #     'service': 'gamerblog',
-        #     'passfile': '.pgpass',
-        # },
-        'NAME': 'gamerblog',
-        'USER': 'postgres',
-        'PASSWORD': 'Genius2008',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
