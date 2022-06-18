bd1baa2e2c01a86fdf11e4187474d3875f3130a1
joseorozco84 <joseorozco84@gmail.com>
2022-06-18 01:52:40 -0300

    blog update

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,18 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    comments = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def summary(self):
         return self.body[:100]
+
+class Comments(models.Model):
+    # user = 
+    comment = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.comment
